c52f5fc9215523d29998e3852cf6fc814058ef49
malidaddydev <elipse@malidaddy.com>
2025-07-21 15:33:51 +0500

    Update Breadcrumbs.jsx

--- src/app/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/app/components/Breadcrumbs/Breadcrumbs.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Breadcrumbs = ({ pageName }) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-[26px] font-bold leading-[30px] text-dark dark:text-white">
+      <h2 className="text-[26px] font-bold leading-[30px] text-[#111928]">
         {pageName}
       </h2>
 
